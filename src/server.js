d779b0c11d2f88acb2a07296e3dd48fa8729d919
Omar Gutierrez <omargtdev@gmail.com>
2023-10-14 10:36:25 -0500

    HOST removed and message changed

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-import { SERVER_HOST, SERVER_PORT } from "./config/env.js";
+import { SERVER_PORT } from "./config/env.js";
 import router from "./routes/index.js";
 import utilMiddleware from "./middlewares/util.middleware.js";
 
@@ -10,7 +10,6 @@ const server = express();
 
 // Variables
 server.set('PORT', SERVER_PORT);
-server.set('HOST', SERVER_HOST);
 
 // Middlewares
 server.use(cors({
@@ -26,7 +25,7 @@ server.use(router);
 server.start = function() {
 
 	const listeningMessage = () => {
-		console.log(`App listening, go to http://${SERVER_HOST}:${SERVER_PORT}`);
+		console.log(`App listening on port ${SERVER_PORT}`);
 	};
 	this.listen(SERVER_PORT, listeningMessage);
 
