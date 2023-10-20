49e7f881fe4d8a5ab639c90a3689b660e178ac94
omargtdev <omargtdev@gmail.com>
2023-10-19 23:14:25 -0500

    Default PORT changed to 8080

--- src/config/env.js
+++ src/config/env.js
@@ -5,7 +5,7 @@ config();
 export const MONGODB_USER = process.env.MONGODB_USER || "";
 export const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || "";
 export const MONGODB_CLUSTER = process.env.MONGODB_CLUSTER || "";
-export const SERVER_PORT = process.env.SERVER_PORT || 4500;
+export const SERVER_PORT = process.env.SERVER_PORT || 8080;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
 export const TOKEN_EXPIRY_TIME = process.env.TOKEN_EXPIRY_TIME || "";
 export const TOKEN_ALGORITHM = process.env.TOKEN_ALGORITHM || "";
